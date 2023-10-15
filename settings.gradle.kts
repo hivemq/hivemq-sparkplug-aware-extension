a258b5e24bab06fd781905d0f498c6fcd3e1281d
Silvio Giebl <silvio.giebl@hivemq.com>
2023-10-15 22:20:03 +0200

    Update hivemq extension gradle plugin to 3.1.0 Replace utf8 with defaults plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,6 @@ pluginManagement {
     plugins {
         id("com.hivemq.extension") version "${extra["plugin.hivemq-extension.version"]}"
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-        id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
+        id("io.github.sgtsilvio.gradle.defaults") version "${extra["plugin.defaults.version"]}"
     }
 }
