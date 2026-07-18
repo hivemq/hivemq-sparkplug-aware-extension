b9116dce42af566a08f8acc905f97132a5dc99ce
David Sondermann <david.sondermann@hivemq.com>
2026-07-18 09:32:02 +0200

    Adapt to the OCI image dependency DSL of gradle-oci 0.28.0
    gradle-oci 0.28.0 removed the OciImageDependencyCollector overloads that
    accept a Project. Use the project() dependency factory instead.

--- build.gradle.kts
+++ build.gradle.kts
@@ -114,7 +114,7 @@ testing {
             }
             oci.of(this) {
                 imageDependencies {
-                    runtime(project).tag("latest")
+                    runtime(project()).tag("latest")
                 }
             }
         }
